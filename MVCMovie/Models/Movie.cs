1661b2dd35b8b7886193e1d6d0b67b7177178b90
nvdiep11062000 <you@example.com>
2021-09-27 08:22:24 +0700

    add search

--- MVCMovie/Models/Movie.cs
+++ MVCMovie/Models/Movie.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,11 +12,15 @@ namespace MVCMovie.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Title is not blank")]
+        [MaxLength(20)]
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
